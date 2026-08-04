7b7dd226ad180f0ca0e2fb165d9ae85c35bff297
Joseph Garcia <garciajoseph517@gmail.com>
2026-08-04 16:38:47 -0400

    style employee directory layout

--- ALAB-320H-1-1-React-Page-Layout/src/components/EmployeeListItem.jsx
+++ ALAB-320H-1-1-React-Page-Layout/src/components/EmployeeListItem.jsx
@@ -1,8 +1,11 @@
 function EmployeeListItem(props) {
   return (
     <div className="employeeListItem">
-      <h2 className="employeeName">{props.employeeName}</h2>
-      <p className="jobTitle">{props.jobTitle}</p>
+      <div className="employeeIcon">👤</div>
+      <div className="employeeInfo">
+        <h2 className="employeeName">{props.employeeName}</h2>
+        <p className="jobTitle">{props.jobTitle}</p>
+      </div>
     </div>
   );
 }
