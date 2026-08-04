734edefcf075f6b9bb0b7c1fac327159ecd4648d
Joseph Garcia <garciajoseph517@gmail.com>
2026-08-04 16:23:33 -0400

    set up react page layout lab

---
+++ ALAB-320H-1-1-React-Page-Layout/README.md
@@ -0,0 +1,5 @@
+# React Page Layout Lab
+
+This project contains ALAB 320H.1.1 React Page Layout.
+
+Run `npm run dev` to start the project.
---
+++ ALAB-320H-1-1-React-Page-Layout/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id="appBody">
+      <h1>React Page Layout Lab</h1>
+    </div>
+  );
+}
+
+export default App;
