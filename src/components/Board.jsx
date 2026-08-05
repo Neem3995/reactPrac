278956cbdfccefeee5f11e3fdc772386987d67f7
Neem3995 <garciajoseph517@gmail.com>
2026-08-05 15:47:47 -0400

    more react notes from 08/05/2026

---
+++ src/components/Board.jsx
@@ -0,0 +1,19 @@
+import Square from './Square.jsx'
+
+function Board (props){
+    return (
+        <div className="board">
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+        </div>
+    );  
+}
+
+export default Board;
